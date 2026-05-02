fd61cff67cda68dcb35a22bb14d16cdd0e0e7375
liamyoum <waiting4corona@gmail.com>
2026-05-02 15:38:39 +0900

    refactor(arg pass): refactored tokenization logic in process_exec()

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -22,6 +22,8 @@
 #include "vm/vm.h"
 #endif
 
+#define MAX_ARGS 63
+
 static void process_cleanup (void);
 static bool load (const char *file_name, struct intr_frame *if_, int argc, char *argv_tokens[]);
 static void initd (void *f_name);
@@ -160,22 +162,29 @@ process_exec (void *f_name) {
 	char *file_name = f_name;
 	bool success;
 	/* 파싱 결과 담을 배열 */
-	char *argv_tokens[64]; //	테스트 케이스 분석 결과, 가장 많은 인자 개수는 23 이지만, 여유 있게 64로 구현
+	char *argv_tokens[MAX_ARGS + 1]; //	테스트 케이스 분석 결과, 가장 많은 인자 개수는 23 이지만, 여유 있게 64로 구현
 	/* strtok_r 함수 사용을 위한 포인터 */
 	char *token;
 	char *next_token;
 	/* argv_tokens에 argument 저장과 argc 계산을 위한 변수 */
 	int argc = 0;
 	/* strtok_r을 이용해 tokenize */
-	token = strtok_r(file_name, " ", &next_token);
-	argv_tokens[argc] = token;
+	for (token = strtok_r (file_name, " ", &next_token);
+			 token != NULL;
+			 token = strtok_r (NULL, " ", &next_token)) {
+		/* argv_tokens[MAX_ARGS]에 접근 전에 반복문이 끝나야 함.
+		 * 그렇지 않으면, 배열의 크기보다 인자 수가 더 많은 것
+		*/		
+		if (argc = MAX_ARGS) {
+			palloc_free_page(file_name);
+			return -1;
+		}
 
-	while(token) {
-		/* 여기서 첫 번째 인자를 NULL로 전달해야, 이미 처리한 문자열을 건너뛰고 처리*/
-		token = strtok_r(NULL, " ", &next_token);
-		argc++;
 		argv_tokens[argc] = token;
+		argc++;
 	}
+	/* 0부터 시작했으니 배열의 argc 번째 요소는 NULL */
+	argv_tokens[argc] = NULL;
 
 	/* thread 구조체의 intr_frame은 사용할 수 없습니다.
 	 * 그 이유는 현재 스레드가 다시 스케줄될 때
