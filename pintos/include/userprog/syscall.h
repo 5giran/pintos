6d0a01e629d57d3e9586a2059e31b3c6ad959944
Giran Oh <giranoh12@gmail.com>
2026-05-05 02:19:13 +0900

    feat(syscall): uaccess.c/h를 syscall.c/h에 병합

--- pintos/include/userprog/syscall.h
+++ pintos/include/userprog/syscall.h
@@ -1,6 +1,17 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+#include <stddef.h>
+
 void syscall_init (void);
+
+bool validate_user_read(const void *buffer, size_t size);
+bool validate_user_write(const void *buffer, size_t size);
+
+
+void copy_in (void *dst, const void *usrc, size_t size);
+void copy_out (void *udst, const void *src, size_t size);
+char *copy_in_string (const char *ustr);
 
 #endif /* userprog/syscall.h */
