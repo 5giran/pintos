2a777e12670180d61f2389792251aac331be2ee1
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-05-04 20:57:31 +0900

    feat(fd): fd 테이블 내 빈 공간 찾는 함수 find_empty_fd() 구현

--- pintos/include/userprog/fd.h
+++ pintos/include/userprog/fd.h
@@ -6,3 +6,5 @@ struct file;
 struct thread;
 
 void fd_init (struct thread *t);       /* 한 프로세스의 fd 테이블을 초기화한다 */
+// int find_empty_fd (struct thread *t);  /* fd 테이블에서 빈 자리를 찾는 함수 */    
+#endif                             // include guard 종료
--- pintos/userprog/fd.c
+++ pintos/userprog/fd.c
@@ -1,11 +1,21 @@
 #include "threads/thread.h"
 #include "userprog/fd.h"
 
-static int
+void
 fd_init (struct thread *t)
 {
     for (int fd = 0; fd < FD_MAX; fd++)     /* 전체 fd 슬롯을 모두 순회한다 */
 		t->fd_table[fd] = NULL;             /* 모든 슬롯을 비어 있는 상태로 만든다 */ 
 	t->next_fd = 2;                         /* 일반 파일 fd는 2번부터 배정 시작한다 */
 	t->running_file = NULL;                 /* 아직 실행 파일을 잡고 있지 않으므로 NULL로 둔다 */ 
+}
+
+int
+find_empty_fd (struct thread *t)
+{
+    for (int i = 0; i < FD_MAX - 2; i--) {
+        int fd = 2 + ((t->next_fd - 2 + i) % (FD_MAX - 2));
+        if (t->fd_table[fd] == NULL)                        // 해당 슬롯이 비어 있다면
+			return fd;     
+    }
 }
