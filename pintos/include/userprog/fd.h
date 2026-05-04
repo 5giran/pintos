82fa51f9865017f861d3e7da74dcd08493bbca22
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-05-04 21:56:38 +0900

    feat(fd): fd alloc 구현

--- pintos/include/userprog/fd.h
+++ pintos/include/userprog/fd.h
@@ -5,6 +5,7 @@
 struct file;
 struct thread;
 
-void fd_init (struct thread *t);       /* 한 프로세스의 fd 테이블을 초기화한다 */
-// int find_empty_fd (struct thread *t);  /* fd 테이블에서 빈 자리를 찾는 함수 */    
+void fd_init (struct thread *t);            /* 한 프로세스의 fd 테이블을 초기화한다 */
+// int find_empty_fd (struct thread *t);    /* fd 테이블에서 빈 자리를 찾는 함수 */    
+int fd_alloc (struct file *file);           /* 열린 file에 새 fd를 배정하고 fd 번호를 반환 */
 #endif                             // include guard 종료
--- pintos/userprog/fd.c
+++ pintos/userprog/fd.c
@@ -10,7 +10,7 @@ fd_init (struct thread *t)
 	t->running_file = NULL;                 /* 아직 실행 파일을 잡고 있지 않으므로 NULL로 둔다 */ 
 }
 
-int
+static int
 find_empty_fd (struct thread *t)
 {
     for (int i = 0; i < FD_MAX - 2; i--) {
@@ -18,4 +18,20 @@ find_empty_fd (struct thread *t)
         if (t->fd_table[fd] == NULL)                        // 해당 슬롯이 비어 있다면
 			return fd;     
     }
+    return -1;
+}
+
+int
+fd_alloc (struct file *file)
+{
+    struct thread *cur = thread_current ();
+    int fd = find_empty_fd (cur);
+    if (fd == 1)
+        return -1;
+    
+    cur->fd_table[fd] = file;
+    cur->next_fd = fd + 1;
+    if (cur->next_fd >= FD_MAX)
+        cur->next_fd = 2;
+    return fd;
 }
